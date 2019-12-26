ead1759984b5fe067cf508e218cb45c0b419d5e9
牵着蜗牛散步 <285062357@qq.com>
2019-12-26 14:42:21 +0800

    123

--- app/src/main/java/com/smh/szyproject/test/invoke/invokeActivity.java
+++ app/src/main/java/com/smh/szyproject/test/invoke/invokeActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import com.smh.szyproject.R;
 import com.smh.szyproject.base.BaseActivity;
+import com.smh.szyproject.utils.ToastUtils;
 
 public class invokeActivity extends BaseActivity {
 
     @Override
     public int getLayoutId() {
+        ToastUtils.showToastForText(this,"123");
         return R.layout.activity_mvvm;
     }
 
