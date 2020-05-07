d6766ed26a6bb6d80f41777248d3855e304cf972
牵着蜗牛散步 <285062357@qq.com>
2020-05-07 16:50:34 +0800

    新增友盟分享、登录和统计功能

---
+++ umeng/src/main/java/com/smh/szyproject/wxapi/WXEntryActivity.java
@@ -0,0 +1,16 @@
+package com.smh.szyproject.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+
+public final class WXEntryActivity extends WXCallbackActivity {
+    /**
+     * 注意事项
+     * 1、umeng的module不能和主app的包名一致
+     * 2、修改混淆命令
+     * 3、修改manifest的包名
+     * 4、key都放到build.gradle里
+     */
+
+
+}
